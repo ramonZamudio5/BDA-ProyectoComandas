a84eac6d2e5a52b1933070739c5459e1adde0780
JoseAguilar05 <jose.aguilar252049@potros.itson.edu.mx>
2025-03-31 17:51:14 -0600

    commit solo de prueba 2

--- ProyectoComandasPresentacion/src/main/java/com/mycompany/proyectocomandaspresentacion/ProyectoComandasPresentacion.java
+++ ProyectoComandasPresentacion/src/main/java/com/mycompany/proyectocomandaspresentacion/ProyectoComandasPresentacion.java
@@ -12,6 +12,6 @@ public class ProyectoComandasPresentacion {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Prueba");
+        System.out.println("Prueba 1");
     }
 }
