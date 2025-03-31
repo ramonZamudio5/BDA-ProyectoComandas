95024b4d88d04ce115fcabc2efe4102f48300428
ramonZamudio5 <ramon.zamudio251924@potros.itson.edu.mx>
2025-03-31 16:23:17 -0700

    pruebas

---
+++ ProyectoComandasPersistencia/src/main/java/com/mycompany/proyectocomandaspersistencia/ProyectoComandasPersistencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectocomandaspersistencia;
+
+/**
+ *
+ * @author Ramón Zamudio
+ */
+public class ProyectoComandasPersistencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ ProyectoComandasPresentacion/src/main/java/com/mycompany/proyectocomandaspresentacion/ProyectoComandasPresentacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectocomandaspresentacion;
+
+/**
+ *
+ * @author Ramón Zamudio
+ */
+public class ProyectoComandasPresentacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
