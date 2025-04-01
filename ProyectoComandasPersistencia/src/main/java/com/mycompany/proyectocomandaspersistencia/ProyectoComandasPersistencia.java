c984aa86689dc4e27e2d4cc293d9db10d2ff698a
janethcrgq <janethcrgq02@gmail.com>
2025-04-01 15:10:14 -0700

    Revert "ajuste"
    This reverts commit 4a9cab7245db02134f37725cd43dee6b0ef5a37c.

--- ProyectoComandasPersistencia/src/main/java/com/mycompany/proyectocomandaspersistencia/ProyectoComandasPersistencia.java
+++ ProyectoComandasPersistencia/src/main/java/com/mycompany/proyectocomandaspersistencia/ProyectoComandasPersistencia.java
@@ -4,21 +4,13 @@
 
 package com.mycompany.proyectocomandaspersistencia;
 
-import daos.ProductoDAO;
-import entidades.Producto;
-import entidades.Tipo;
-import excepciones.AgregarProductoException;
-
 /**
  *
  * @author Ramón Zamudio
  */
 public class ProyectoComandasPersistencia {
 
-    public static void main(String[] args) throws AgregarProductoException {
-        Producto prod = new Producto("sshs", 5.0, Tipo.POSTRE);
-        ProductoDAO dao = ProductoDAO.getInstance();
-        dao.agregarProducto(prod);
-        
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
--- ProyectoComandasPersistencia/src/main/java/conexion/Conexion.java
+++ ProyectoComandasPersistencia/src/main/java/conexion/Conexion.java
@@ -12,7 +12,7 @@ import javax.persistence.Persistence;
  */
 
 public class Conexion {
-    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("ConexionPU");
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("conexionPU");
 
     public static EntityManager crearConexion() {
         return emf.createEntityManager();
