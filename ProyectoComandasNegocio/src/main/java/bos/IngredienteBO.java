b180c22e256e1d08ba1eefe0704319fe86d29cf6
janethcrgq <janethcrgq02@gmail.com>
2025-04-02 16:10:35 -0700

    mapper y bo

--- ProyectoComandasNegocio/src/main/java/bos/IngredienteBO.java
+++ ProyectoComandasNegocio/src/main/java/bos/IngredienteBO.java
@@ -4,8 +4,14 @@
  */
 package bos;
 
+import dtos.IngredienteDTO;
+import entidades.Ingrediente;
+import excepciones.BuscarPorNombreException;
+import excepciones.NegocioException;
 import interfaces.IIngredienteBO;
 import interfaces.IIngredienteDAO;
+import java.util.List;
+import mappers.IngredienteMapper;
 
 /**
  *
@@ -18,6 +24,29 @@ public class IngredienteBO implements IIngredienteBO {
     public IngredienteBO(IIngredienteDAO ingredienteDAO){
         this.ingredienteDAO= ingredienteDAO;
     }
+    
+    public List<IngredienteDTO> buscarPorNombre(String nombre) throws NegocioException, BuscarPorNombreException{
+        if (nombre==null || nombre.trim().isEmpty()){
+            throw new BuscarPorNombreException("Ingredientes con el nombre "+nombre+ " no encontrados");
+        }
+        try{
+            List<Ingrediente> ingredientes= ingredienteDAO.buscarPorNombre(nombre);
+            if (ingredientes==null || ingredientes.isEmpty()){
+                throw new BuscarPorNombreException("Ingredientes no encontrados con el nombre "+ nombre);
+            }
+            for(Ingrediente ingrediente: ingredientes){
+                if(ingrediente.getId()==null){
+                    throw new NegocioException("El ingrediente no puede ser nulo");
+                }
+            }
+            return IngredienteMapper.toListDTO(ingredientes);
+        } catch(Exception e){
+            throw new NegocioException("Error al buscar ingrediente por nombre");
+            
+        }
+    }
+    
+    
     
     
     
---
+++ ProyectoComandasNegocio/src/main/java/mappers/IngredienteMapper.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mappers;
+
+import dtos.IngredienteDTO;
+import entidades.Ingrediente;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author janethcristinagalvanquinonez
+ */
+public class IngredienteMapper {
+    
+    public static IngredienteDTO toDTO(Ingrediente ingrediente){
+        if(ingrediente==null){
+            return null;
+        }
+        return new IngredienteDTO(
+                ingrediente.getNombre(),
+                ingrediente.getStock(),
+                ingrediente.getUnidadMedida()
+        );
+        
+    }
+    
+    public static List<IngredienteDTO> toListDTO(List<Ingrediente> ingredientes) {
+        if (ingredientes == null || ingredientes.isEmpty()) {
+            return null;
+        }
+        return ingredientes.stream()
+                .map(IngredienteMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+    
+    public static Ingrediente toEntity(IngredienteDTO ingredienteDTO){
+        if (ingredienteDTO==null){
+            return null;
+        } return new Ingrediente(
+                ingredienteDTO.getNombre(), 
+                ingredienteDTO.getStock(), 
+                ingredienteDTO.getUnidadMedida()
+        );
+    }
+    
+    
+    
+    
+}
