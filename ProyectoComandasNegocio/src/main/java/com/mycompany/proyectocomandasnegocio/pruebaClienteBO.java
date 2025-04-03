1fa4f40b7bb3100ca9fef6313c2c268348598afc
JoseAguilar05 <jose.aguilar252049@potros.itson.edu.mx>
2025-04-02 20:52:48 -0700

    main para probar mi bo
    sin codigo

---
+++ ProyectoComandasNegocio/src/main/java/com/mycompany/proyectocomandasnegocio/pruebaClienteBO.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.proyectocomandasnegocio;
+
+import dtos.ClienteFrecuenteDTO;
+import excepciones.AgregarClienteFrecuenteException;
+import excepciones.BuscarClienteFrecuenteException;
+import excepciones.NegocioException;
+import interfaces.IClienteFrecuenteBO;
+import interfaces.IClienteFrecuenteDAO;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+/**
+ *
+ * @author Cricri
+ */
+public class pruebaClienteBO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)  {
+  
+}
+}
