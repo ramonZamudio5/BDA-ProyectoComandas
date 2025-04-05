ca93c2626d16476a3126d5c7eca4640ebb0cb18f
janethcrgq <janethcrgq02@gmail.com>
2025-04-04 18:17:26 -0700

    seleccion de filtro BuscarIngredientes

--- ProyectoComandasPresentacion/src/main/java/Interfaces/BuscarIngrediente.java
+++ ProyectoComandasPresentacion/src/main/java/Interfaces/BuscarIngrediente.java
@@ -4,6 +4,14 @@
  */
 package Interfaces;
 
+import java.awt.CardLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import  enums.UnidadMedida;
+
 /**
  *
  * @author janethcristinagalvanquinonez
@@ -13,10 +21,31 @@ public class BuscarIngrediente extends javax.swing.JFrame {
     /**
      * Creates new form BuscarIngrediente
      */
+    private CardLayout cardLayout;
+        JComboBox<String> comboUnidad= new JComboBox<>();
     public BuscarIngrediente() {
         initComponents();
+        ButtonGroup buttonGroup= new ButtonGroup();
+        buttonGroup.add(botonMedida);
+        buttonGroup.add(botonNombre);
+         cardLayout= new CardLayout();
+        panelBusqueda.setLayout(cardLayout);
+        JPanel panelNombre= new JPanel();
+        
+        JTextField campoNombre= new JTextField(15);
+        panelNombre.add(new JLabel("Nombre..."));
+        panelNombre.add(campoNombre);
+        
+        JPanel panelUnidad= new JPanel();
+      //  JComboBox<String> comboUnidad= new JComboBox<>();
+        panelUnidad.add(comboUnidad);
+        
+        panelBusqueda.add(panelNombre, "nombre");
+        panelBusqueda.add(panelUnidad, "unidad");
+        
+        
     }
-
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -28,6 +57,11 @@ public class BuscarIngrediente extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jPanel2 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        panelFiltro = new javax.swing.JPanel();
+        botonMedida = new javax.swing.JRadioButton();
+        botonNombre = new javax.swing.JRadioButton();
+        panelBusqueda = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -39,11 +73,60 @@ public class BuscarIngrediente extends javax.swing.JFrame {
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 420, Short.MAX_VALUE)
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 27, Short.MAX_VALUE)
+        );
+
+        jLabel1.setFont(new java.awt.Font("Gujarati MT", 0, 18)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(0, 153, 0));
+        jLabel1.setText("BUSCAR INGREDIENTE");
+
+        botonMedida.setText("UNIDAD DE MEDIDA");
+        botonMedida.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonMedidaActionPerformed(evt);
+            }
+        });
+
+        botonNombre.setText("NOMBRE");
+        botonNombre.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonNombreActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout panelFiltroLayout = new javax.swing.GroupLayout(panelFiltro);
+        panelFiltro.setLayout(panelFiltroLayout);
+        panelFiltroLayout.setHorizontalGroup(
+            panelFiltroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelFiltroLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(botonNombre)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(botonMedida)
+                .addContainerGap())
+        );
+        panelFiltroLayout.setVerticalGroup(
+            panelFiltroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelFiltroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addComponent(botonMedida)
+                .addComponent(botonNombre))
+        );
+
+        panelBusqueda.setBackground(new java.awt.Color(255, 255, 255));
+
+        javax.swing.GroupLayout panelBusquedaLayout = new javax.swing.GroupLayout(panelBusqueda);
+        panelBusqueda.setLayout(panelBusquedaLayout);
+        panelBusquedaLayout.setHorizontalGroup(
+            panelBusquedaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 286, Short.MAX_VALUE)
+        );
+        panelBusquedaLayout.setVerticalGroup(
+            panelBusquedaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 30, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
@@ -51,12 +134,31 @@ public class BuscarIngrediente extends javax.swing.JFrame {
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 289, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(19, 19, 19))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(panelFiltro, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(92, 92, 92))))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(56, 56, 56)
+                .addComponent(panelBusqueda, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 273, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(panelFiltro, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(panelBusqueda, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 167, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -72,6 +174,18 @@ public class BuscarIngrediente extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void botonMedidaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonMedidaActionPerformed
+        cardLayout.show(panelBusqueda, "unidad");
+        comboUnidad.removeAllItems();
+        for(UnidadMedida unidad : UnidadMedida.values()){
+            comboUnidad.addItem(unidad.name());
+        }
+    }//GEN-LAST:event_botonMedidaActionPerformed
+
+    private void botonNombreActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonNombreActionPerformed
+        cardLayout.show(panelBusqueda, "nombre");
+    }//GEN-LAST:event_botonNombreActionPerformed
 
     /**
      * @param args the command line arguments
@@ -109,7 +223,12 @@ public class BuscarIngrediente extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JRadioButton botonMedida;
+    private javax.swing.JRadioButton botonNombre;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel panelBusqueda;
+    private javax.swing.JPanel panelFiltro;
     // End of variables declaration//GEN-END:variables
 }
