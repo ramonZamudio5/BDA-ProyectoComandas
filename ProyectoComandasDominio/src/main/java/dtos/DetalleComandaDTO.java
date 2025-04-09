536a275f742b6c80cbc44cddd285629451525465
JoseAguilar05 <jose.aguilar252049@potros.itson.edu.mx>
2025-04-09 16:40:34 -0700

    detalleComandaDTO

--- ProyectoComandasDominio/src/main/java/dtos/DetalleComandaDTO.java
+++ ProyectoComandasDominio/src/main/java/dtos/DetalleComandaDTO.java
@@ -4,10 +4,99 @@
  */
 package dtos;
 
+import entidades.Producto;
+
 /**
  *
  * @author Ramón Zamudio
  */
 public class DetalleComandaDTO {
+     private Long id;
     
+     private Producto producto;
+     
+     private int cantidad;
+     
+     private double precioUnitario;
+     
+     private double importe;
+     
+     private String notas;
+
+    public DetalleComandaDTO() {
+    }
+
+    public DetalleComandaDTO(Long id, Producto producto, int cantidad, double precioUnitario, double importe, String notas) {
+        this.id = id;
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+        this.importe = importe;
+        this.notas = notas;
+    }
+
+    public DetalleComandaDTO(Producto producto, int cantidad, double precioUnitario, double importe, String notas) {
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+        this.importe = importe;
+        this.notas = notas;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(double precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public double getImporte() {
+        return importe;
+    }
+
+    public void setImporte(double importe) {
+        this.importe = importe;
+    }
+
+    public String getNotas() {
+        return notas;
+    }
+
+    public void setNotas(String notas) {
+        this.notas = notas;
+    }
+
+    @Override
+    public String toString() {
+        return "DetalleComandaDTO{" + "id=" + id + ", producto=" + producto + ", cantidad=" + cantidad + ", precioUnitario=" + precioUnitario + ", importe=" + importe + ", notas=" + notas + '}';
+    }
+     
+     
+     
+     
 }
